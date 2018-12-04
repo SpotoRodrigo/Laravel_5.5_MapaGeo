766a6ff0b7fc682ab63e022a70da9e501b4dd771
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2018-12-04 15:13:34 -0200

    itatiba2

--- app/Jobs/ProcessItatiba.php
+++ app/Jobs/ProcessItatiba.php
@@ -48,7 +48,7 @@ class ProcessItatiba implements ShouldQueue
      */
     public function handle()
     {
-
+       dd( $this->nome_arquivo );
       // VERIFICO SE EXISTE REGISTRO NO BANCO O ARQUIVO EM PROCESSO.  
 /*
       $lista = DB::connection('BDGeralItatiba')->select("SELECT  CASE WHEN CHARINDEX ('_',imagemnome) =0 THEN  SUBSTRING (imagemnome , 0 ,  CHARINDEX ('.',imagemnome) ) 
@@ -58,8 +58,6 @@ class ProcessItatiba implements ShouldQueue
          GROUP BY CASE WHEN CHARINDEX ('_',imagemnome) =0 THEN  SUBSTRING (imagemnome , 0 ,  CHARINDEX ('.',imagemnome) ) 
         ELSE  SUBSTRING (imagemnome , 0 ,  CHARINDEX ('_',imagemnome) )  END " ,[$this->nome_arquivo] );
 */
-
-        dd($this->nome_arquivo );
 $lista = DB::connection('BDGeralItatiba')->select("SELECT top 1 keyfoto AS inscricao FROM dbo.Imagem WHERE imagemNomeAnterior = ? AND TipoFoto = 'Foto Fachada'",$this->nome_arquivo] );
 
 dd($lista);
