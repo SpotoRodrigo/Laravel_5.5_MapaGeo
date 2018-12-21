5857959cb3daef48dce346ef5d315d5dd051c755
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2018-12-21 17:46:11 -0200

    ajuste

--- app/Http/Controllers/Ups3Controller.php
+++ app/Http/Controllers/Ups3Controller.php
@@ -33,127 +33,130 @@ class Ups3Controller extends Controller
      //   $lista = DB::connection('BDGeralLorenaImagem')->select("SELECT top 3 SUBSTRING(imagemNomeAnterior,1,16)  AS inscricao   , COUNT(CodImagem) as qtde FROM dbo.Imagem GROUP BY SUBSTRING(imagemNomeAnterior,1,16) "  );
      //   dd($lista );
         
-        $lista =  DB::connection('BDServicoVinhedo')->select("SELECT cnhIdentificador as idd
-        ,cnhImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+cnhImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_CNH' as tabela
-    FROM LOG_documentos.cnh as cnh
+        $lista =  DB::connection('BDServicoVinhedo')->select(" SELECT cnhIdentificador as idd
+            ,cnhImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+cnhImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_CNH' as tabela
+            , cnh.imagemS3
+        FROM LOG_documentos.cnh as cnh
         , pessoa.Fisica  as peso
-    where cnhImagem is not null  AND cnh.imagemS3 is null AND cnhImagem <> ''
-    AND cnh.cnhPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-    UNION 
- SELECT TituloIdentificador as idd
-        ,TituloImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+TituloImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_TITULO' as tabela
-    FROM LOG_documentos.TituloEleitor as Titulo
+        where cnhImagem is not null  AND cnh.imagemS3 is not null AND cnhImagem <> ''
+        AND cnh.cnhPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        
+        UNION 
+        SELECT TituloIdentificador as idd
+            ,TituloImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+TituloImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_TITULO' as tabela
+            , Titulo.imagemS3
+        FROM LOG_documentos.TituloEleitor as Titulo
         , pessoa.Fisica  as peso
-    where TituloImagem is not null  AND Titulo.imagemS3 is null AND TituloImagem <> ''
-    AND Titulo.TituloPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-UNION 
- SELECT CertidaoIdentificador as idd
-        ,CertidaoImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CertidaoImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_CERTIDAO' as tabela
-    FROM LOG_documentos.Certidao as Certidao
+        where TituloImagem is not null  AND Titulo.imagemS3 is not null AND TituloImagem <> ''
+        AND Titulo.TituloPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        UNION 
+        SELECT CertidaoIdentificador as idd
+            ,CertidaoImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CertidaoImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_CERTIDAO' as tabela
+            , Certidao.imagemS3
+        FROM LOG_documentos.Certidao as Certidao
         , pessoa.Fisica  as peso
-    where CertidaoImagem is not null  AND Certidao.imagemS3 is null AND CertidaoImagem <> ''
-    AND Certidao.CertidaoPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-
-union 
- SELECT RgIdentificador as idd
-        ,RgImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+RgImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_RG' as tabela
-     FROM LOG_documentos.Rg as Rg
-        , pessoa.Fisica  as peso
-    where RgImagem is not null  AND Rg.imagemS3 is null AND RgImagem <> ''
-    AND Rg.RgPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-
-UNION 
-
-Select   emd.enderecoIdentificador as idd 
-, enderecoImagem   as imagem 
-,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+enderecoImagem  as url_image
-,CAST( fi.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-, 'LOG_ENDERECO' as tabela
-
-from log_pessoa.PessoaEndereco  as emd
-, pessoa.fisica  as fi
-where emd.enderecoPessoaFisicaIdentificador = fi.pessoaFisicaIdentificador
-and emd.enderecoImagem is not null and emd.enderecoImagem <> ''  and emd.imagemS3 is  null  
-
-union 
-
-Select   fi.pessoaFisicaIdentificador as idd 
-, fi.pessoaFisicaFoto   as imagem 
-,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+ fi.pessoaFisicaFoto  as url_image
-,CAST( fiii.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-, 'LOG_PESSOA' as tabela
-
-from  log_pessoa.fisica  as fi
-, pessoa.fisica as fiii
-where fi.pessoaFisicaFoto is not null and fi.pessoaFisicaFoto <> ''  and fi.imagemS3 is  null  
-AND fi.pessoaFisicaFisicaIdentificador = fiii.pessoaFisicaIdentificador
-
-
-UNION     
-
-SELECT CartaoCidadaoIdentificador as idd
-        ,CartaoCidadaoImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CartaoCidadaoImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_CIDADAO' as tabela
- FROM LOG_documentos.CartaoCidadao as cnh
-        , pessoa.Fisica  as peso
-    where CartaoCidadaoImagem is not null  AND cnh.imagemS3 is null AND CartaoCidadaoImagem <> ''
-    AND cnh.CartaoCidadaoPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-    UNION  
- SELECT ReservistaIdentificador as idd  
-        ,ReservistaImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+ReservistaImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_RESERVISTA' as tabela
-    FROM LOG_documentos.CarteiraReservista as Titulo
-        , pessoa.Fisica  as peso
-    where ReservistaImagem is not null  AND Titulo.imagemS3 is null AND ReservistaImagem <> ''
-    AND Titulo.ReservistaPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-UNION 
- SELECT CnsIdentificador as idd
-        ,CnsImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CnsImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_CNS' as tabela
-    FROM LOG_documentos.Cns as Certidao
-        , pessoa.Fisica  as peso
-    where CnsImagem is not null  AND Certidao.imagemS3 is null AND CnsImagem <> ''
-    AND Certidao.CnsPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-
-union  
- SELECT CpfIdentificador as idd
-        ,CpfImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CpfImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_CPF' as tabela
-     FROM LOG_documentos.Cpf as Rg
-        , pessoa.Fisica  as peso
-    where CpfImagem is not null  AND Rg.imagemS3 is null AND CpfImagem <> ''
-    AND Rg.CpfPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
-
+        where CertidaoImagem is not null  AND Certidao.imagemS3 is not null AND CertidaoImagem <> ''
+        AND Certidao.CertidaoPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        
         union 
- SELECT CtpsIdentificador as idd
-        ,CtpsImagem
-        ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CtpsImagem  as url_image
-        ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
-        , 'LOG_CTPS' as tabela
-    FROM LOG_documentos.Ctps as Rg
+        SELECT RgIdentificador as idd
+            ,RgImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+RgImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_RG' as tabela
+            , Rg.imagemS3
+        FROM LOG_documentos.Rg as Rg
         , pessoa.Fisica  as peso
-    where CtpsImagem is not null  AND Rg.imagemS3 is null AND CtpsImagem <> ''
-    AND Rg.CtpsPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador " );  // AND cpf.imagemS3 is null
+        where RgImagem is not null  AND Rg.imagemS3 is not null AND RgImagem <> ''
+        AND Rg.RgPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+      
+        UNION 
+        Select   emd.enderecoIdentificador as idd 
+            , enderecoImagem   as imagem 
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+enderecoImagem  as url_image
+            ,CAST( fi.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_ENDERECO' as tabela
+            , emd.imagemS3
+        from log_pessoa.PessoaEndereco  as emd
+        , pessoa.fisica  as fi
+        where emd.enderecoPessoaFisicaIdentificador = fi.pessoaFisicaIdentificador
+        and emd.enderecoImagem is not null and emd.enderecoImagem <> ''  and emd.imagemS3 is  not null  
+        union 
+        Select   fi.pessoaFisicaIdentificador as idd 
+            , fi.pessoaFisicaFoto   as imagem 
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+ fi.pessoaFisicaFoto  as url_image
+            ,CAST( fiii.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_PESSOA' as tabela
+            , fi.imagemS3
+        from  log_pessoa.fisica  as fi
+        , pessoa.fisica as fiii
+        where fi.pessoaFisicaFoto is not null and fi.pessoaFisicaFoto <> ''  and fi.imagemS3 is not null  
+        AND fi.pessoaFisicaFisicaIdentificador = fiii.pessoaFisicaIdentificador
+        UNION     
+        SELECT CartaoCidadaoIdentificador as idd
+            ,CartaoCidadaoImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CartaoCidadaoImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_CIDADAO' as tabela
+            , cnh.imagemS3
+        FROM LOG_documentos.CartaoCidadao as cnh
+        , pessoa.Fisica  as peso
+        where CartaoCidadaoImagem is not null  AND cnh.imagemS3 is not null AND CartaoCidadaoImagem <> ''
+        AND cnh.CartaoCidadaoPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        UNION  
+        SELECT ReservistaIdentificador as idd  
+            ,ReservistaImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+ReservistaImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_RESERVISTA' as tabela
+            , Titulo.imagemS3
+        FROM LOG_documentos.CarteiraReservista as Titulo
+        , pessoa.Fisica  as peso
+        where ReservistaImagem is not null  AND Titulo.imagemS3 is  not null AND ReservistaImagem <> ''
+        AND Titulo.ReservistaPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        UNION 
+        SELECT CnsIdentificador as idd
+            ,CnsImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CnsImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_CNS' as tabela
+            , Certidao.imagemS3
+        FROM LOG_documentos.Cns as Certidao
+        , pessoa.Fisica  as peso
+        where CnsImagem is not null  AND Certidao.imagemS3 is  not null AND CnsImagem <> ''
+        AND Certidao.CnsPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        union  
+        SELECT CpfIdentificador as idd
+            ,CpfImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CpfImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_CPF' as tabela
+            , Rg.imagemS3
+        FROM LOG_documentos.Cpf as Rg
+        , pessoa.Fisica  as peso
+        where CpfImagem is not null  AND Rg.imagemS3 is not null AND CpfImagem <> ''
+        AND Rg.CpfPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+        union 
+        SELECT CtpsIdentificador as idd
+            ,CtpsImagem
+            ,'https://www.mitraonline.com.br/central/modulos/atendimento/arquivos/'+CtpsImagem  as url_image
+            ,CAST( peso.pessoaFisicaIdentificadorUnico AS VARCHAR(MAX) )  as dono
+            , 'LOG_CTPS' as tabela
+            , Rg.imagemS3
+        FROM LOG_documentos.Ctps as Rg
+        , pessoa.Fisica  as peso
+        where CtpsImagem is not null  AND Rg.imagemS3 is  not null AND CtpsImagem <> ''
+        AND Rg.CtpsPessoaFisicaIdentificador = peso.pessoaFisicaIdentificador
+         " );  // AND cpf.imagemS3 is null
 
         //dd($lista );
          foreach ($lista as $file) {
@@ -187,8 +190,9 @@ union
 
                 $nome_completo =  $dono . '/' . $novo_nome . '.jpg' ;
 
+                Storage::disk('s3Vinhedo')->delete($file->imagemS3 );
 
-                $this->dispatch(new upVinhedoDoc($id, $nome_completo ,$url_image , strval($file->tabela) ));  
+                // $this->dispatch(new upVinhedoDoc($id, $nome_completo ,$url_image , strval($file->tabela) ));  
 
 /*
               $novo_nome = $this->uuid();
