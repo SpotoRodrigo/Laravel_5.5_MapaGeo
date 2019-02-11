b2fce8157029a50bae2dd391f1ce91c3556a079e
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2019-02-11 18:19:30 -0200

    novos teste

--- app/Http/Controllers/Ups3Controller.php
+++ app/Http/Controllers/Ups3Controller.php
@@ -455,7 +455,7 @@ class Ups3Controller extends Controller
                                                             AND ImagemFoto is not null" );  // AND cpf.imagemS3 is null
 
 
-    header('Content-Type: image/x-bmp');
+    //header('Content-Type: image/x-bmp');
     //echo $lista->imagemFoto;
         dd($lista );
        $conteudo  =  $lista->imagemFoto ;
