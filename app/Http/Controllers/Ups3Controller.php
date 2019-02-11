f32b542a91bbae73265b6959176865679eadfe10
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2019-02-11 18:05:07 -0200

    novos teste

--- app/Http/Controllers/Ups3Controller.php
+++ app/Http/Controllers/Ups3Controller.php
@@ -455,7 +455,7 @@ class Ups3Controller extends Controller
                                                             AND ImagemFoto is not null" );  // AND cpf.imagemS3 is null
 
        // dd($lista );
-       $conteudo  =  file_get_contents( $lista->imagemFoto ) ;
+       $conteudo  =  file_get_contents(  base64_encode($lista->imagemFoto) ) ;
 
         dd(  `<img src="data:image/jpg;base64,<?=$conteudo?>" /> `  );
 
