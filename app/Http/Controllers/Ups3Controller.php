494a3286faf31c43260236c96e702dff93185172
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2019-02-19 09:33:12 -0300

    ajuste

--- app/Http/Controllers/Ups3Controller.php
+++ app/Http/Controllers/Ups3Controller.php
@@ -768,7 +768,7 @@ class Ups3Controller extends Controller
         foreach ($files as $file) {
             $subiu = false;
             //$lista = DB::connection('BDGeralVinhedoImagem')->select("SELECT  CAST( serv.servicoIdentificadorUnico as   char(50)  )  as idserv , CAST(fich.codFichaIdentUnico as   char(50)  )     as idfile  , CAST( imag.idunico as  char(50) )  AS idimag , ImagemNome   , codImagem FROM BDGeralVinhedoImagem.dbo.Imagem       as imag  , BDGeralVinhedo.habitacao.FichaHabitacao  as fich  , BDServicoVinhedo.organizacao.Servico     as serv WHERE imag.TipoFoto = 'Documento' AND imag.assunto = 'Habitacao' AND imag.ImagemNome  = ? AND fich.codFicha = imag.keyFotoNumerica and serv.servicoIndetificador = 19 order by imag.ImagemNome  " ,[$file->getFilename()] );
-            $lista = DB::connection('BDGeralVinhedoImagem')->select(" select ImagemNome, codImagem ,  uidarquivo , uidficha ,  uidserv   from dbo.viewDocHabitacao WHERE  ImagemNome  = ?  ",[$file->getFilename()] );
+            $lista = DB::connection('BDGeralVinhedo')->select(" select ImagemNome, codImagem ,  uidarquivo , uidficha ,  uidserv   from BDGeralVinhedoImagem.dbo.viewDocHabitacao WHERE  ImagemNome  = ?  ",[$file->getFilename()] );
 dd($lista );
             if($lista  != []  ){
                 $idd = $lista[0]->codImagem;
