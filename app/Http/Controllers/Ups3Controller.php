55a164b51ef967caca44de11d5735e01f4a72583
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2019-03-01 13:31:11 -0300

    campos foto

--- app/Http/Controllers/Ups3Controller.php
+++ app/Http/Controllers/Ups3Controller.php
@@ -38,9 +38,9 @@ class Ups3Controller extends Controller
     {
         // $images = $this->loopPorPastaHabitacao();    //  $this->loopPorPastaQuestionario();    // $this->loopPorPastaEmpresaFacil();  //  $this->loopPorPasta(); 
          
-        $images = $this->loopPorPasta(); 
+        //$images = $this->loopPorPasta(); 
 
-        //$images = $this->loopBucket('s3Campos');
+        $images = $this->loopBucket('s3Campos');
 
 /*
         $buckets = ['s3Paraiso','s3Biri','s3Lorena','s3Itatiba','s3Artur','s3Registro','s3Socorro','s3Slserra','s3Vinhedo','s3Ibitinga'];
@@ -156,8 +156,8 @@ class Ups3Controller extends Controller
 
     private function loopPorPasta()
     {
-        //$directory = "/media/geoserver/transferencias/campos/fotosfachada" ;
-        $directory = "/media/geoserver/transferencias/campos/teste" ;
+        $directory = "/media/geoserver/transferencias/campos/fotosfachada" ;
+        //$directory = "/media/geoserver/transferencias/campos/teste" ;
         //$directory = "/media/geoserver/transferencias/paraiso/Entregavel_06";
         $count= 0;
        
@@ -181,11 +181,11 @@ class Ups3Controller extends Controller
                 'up'      => true
             ];
 
-            if(is_file($file->getRealPath()) ){
+           // if(is_file($file->getRealPath()) ){
                   // $this->dispatch(new ProcessParaiso($file->getExtension() , $file->getFilename() , $file->getRealPath()  ));   // $file->getRealPath()     $conteudo
-                  $this->dispatch(new ProcessCampos($file->getExtension() , $file->getFilename() , $file->getRealPath()  ,  $this->uuid() ));   // $file->getRealPath()     $conteudo
-            }
-          /*
+                  //$this->dispatch(new ProcessCampos($file->getExtension() , $file->getFilename() , $file->getRealPath()  ,  $this->uuid() ));   // $file->getRealPath()     $conteudo
+           // }
+          
             //  CAMPOS   
             if( is_file($file->getRealPath()) ){
 
@@ -241,7 +241,7 @@ class Ups3Controller extends Controller
                             }
                         }
                         
-            }*/
+            }
 
         }
         return $images ;
