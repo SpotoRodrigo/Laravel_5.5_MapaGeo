265481c8ab2ce8ead02c803a1bf2ebd642945f77
Rodrigo Spoto <spoto.rodrigo@mitra.aqa.local>
2019-04-05 18:20:23 -0300

    api consumer

--- app/Http/Controllers/DepositoController.php
+++ app/Http/Controllers/DepositoController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Deposito;
 use Illuminate\Http\Request;
 
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+
 class DepositoController extends Controller
 {
     /**
@@ -117,7 +121,70 @@ class DepositoController extends Controller
 
     public function sincronizar(Request $Request)
     {
-        dd( Deposito::SERVICOS[$Request->input('select')]);
+        // dd( Deposito::SERVICOS[$Request->input('select')]);
+
+       // $token = $this->postRequest()->{'access_token'} ;
+       //  dd( $this->getRequest($token)  );
+
+       $this->teste();
+
+    }
+
+
+/*
+[{"key":"client_id","value":"200b04dcdde546049e11054d6ce95f7a","description":"","type":"text","enabled":true},
+ {"key":"grant_type","value":"password","description":"","type":"text","enabled":true},
+ {"key":"scope","value":"FullControl","description":"","type":"text","enabled":true},
+ {"key":"username","value":"mitraintegrador","description":"","type":"text","enabled":true},
+ {"key":"password","value":"1q2w3e4r5t","description":"","type":"text","enabled":true}]
+*/
+
+    public function postRequest()
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', 'http://integrador4r.4rsistemas.com.br:91/oauth/access_token', [
+            'form_params' => [
+                'client_id' => '200b04dcdde546049e11054d6ce95f7a',
+                'grant_type' => 'password',
+                'scope' => 'FullControl',
+                'username' => 'mitraintegrador',
+                'password' => '1q2w3e4r5t',
+            ]
+        ]);
+        $response = $response->getBody()->getContents();
+        
+        return json_decode($response);
+    }
+
+    public function getRequest(string $token)
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $request = $client->get('http://integrador4r.4rsistemas.com.br:91/rest/tb_bairro/1',[
+            'headers' => [
+                'Authorization' => 'OAuth ' . $token,        
+                'Accept'        => 'application/json',
+            ]
+        ]);  // strtolower(servico)
+        $response = $request->getBody()->getContents();
+
+        return json_decode($response);        
+    }
+
+    public function teste()
+    {
+
+        //fc09aaca62a50365c45c00d9b48c9b67 
+        // Meu ip é 177.18.195.146
+
+        $client = new Client();
+        $query_string="http://api.ipstack.com/177.18.195.146?access_key=fc09aaca62a50365c45c00d9b48c9b67";
+        $iplocation = $client->get($query_string); //this will be guzzle object
+
+        $iplocation_jsoned=json_decode((string)$client->get($query_string)->getBody()); //this will be a json object
+
+        var_dump($iplocation); //this will dump the result object
+        dd($iplocation_jsoned); //this will dump the json object
     }
 
 }
